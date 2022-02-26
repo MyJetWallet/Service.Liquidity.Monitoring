c8dcbbdce06c28d68f825fee664704b42b4cadfa
Maxim Gorin <mxmgorin@gmail.com>
2022-02-26 17:21:27 +0200

    Refactoring

--- src/Service.Liquidity.Monitoring.Domain.Models/RuleSets/MonitoringRule.cs
+++ src/Service.Liquidity.Monitoring.Domain.Models/RuleSets/MonitoringRule.cs
@@ -17,7 +17,7 @@ namespace Service.Liquidity.Monitoring.Domain.Models.RuleSets
         [DataMember(Order = 2)] public IEnumerable<string> CheckIds { get; set; }
         [DataMember(Order = 3)] public LogicalOperatorType LogicalOperatorType { get; set; }
         [DataMember(Order = 4)] public string NotificationChannel { get; set; }
-        [DataMember(Order = 5)] public MonitoringRuleAction MonitoringRuleAction { get; set; }
+        [DataMember(Order = 5)] public MonitoringRuleAction Action { get; set; }
         [DataMember(Order = 6)] public MonitoringRuleState PrevState { get; set; }
         [DataMember(Order = 7)] public MonitoringRuleState CurrentState { get; set; }
         [DataMember(Order = 8)] public string Id { get; set; }
