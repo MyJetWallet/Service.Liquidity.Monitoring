4dca51c057d8f806aa9344515ee02c91a7662f4a
Maxim Gorin <mxmgorin@gmail.com>
2022-03-10 12:11:07 +0200

    Add hedge trade message

---
+++ src/Service.Liquidity.Monitoring.Domain.Models/Hedging/HedgeTradeMessage.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace Service.Liquidity.Monitoring.Domain.Models.Hedging
+{
+    [DataContract]
+    public class HedgeTradeMessage
+    {
+        public const string SbTopicName = "jetwallet-liquidity-hedge-trade-message";
+
+        [DataMember(Order = 1)] public long HedgeStamp { get; set; }
+        [DataMember(Order = 2)] public string BaseAsset { get; set; }
+        [DataMember(Order = 3)] public string BaseWalletId { get; set; }
+        [DataMember(Order = 4)] public decimal BaseVolume { get; set; }
+        [DataMember(Order = 5)] public string QuoteAsset { get; set; }
+        [DataMember(Order = 6)] public string QuoteWalletId { get; set; }
+        [DataMember(Order = 7)] public decimal QuoteVolume { get; set; }
+    }
+}
