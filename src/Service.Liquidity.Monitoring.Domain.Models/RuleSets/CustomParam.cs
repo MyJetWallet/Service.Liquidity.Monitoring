ce51dbcc69e94b06403804dcb5115d8f66003576
Maxim Gorin <mxmgorin@gmail.com>
2022-03-19 16:06:42 +0200

    Fix grpc contract

--- src/Service.Liquidity.Monitoring.Domain.Models/RuleSets/CustomParam.cs
+++ src/Service.Liquidity.Monitoring.Domain.Models/RuleSets/CustomParam.cs
@@ -1,10 +1,12 @@
-﻿
+﻿using System.Runtime.Serialization;
+
 namespace Service.Liquidity.Monitoring.Domain.Models.RuleSets
 {
+    [DataContract]
     public class CustomParam
     {
-        public string Name { get; set; }
-        public string Value { get; set; }
+        [DataMember(Order = 1)] public string Name { get; set; }
+        [DataMember(Order = 2)] public string Value { get; set; }
 
         public string GetString()
         {
